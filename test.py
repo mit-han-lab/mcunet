6a638bbd5edd2ed74413d03bf9a0e00f287c2956
Ji Lin <jilin@mit.edu>
2022-06-15 11:38:15 -0400

    remove test

--- test.py
+++
@@ -1,9 +0,0 @@
-from mcunet.model_zoo import build_model, model_id_list
-
-
-print(model_id_list)
-for model_id in model_id_list:
-    model, resolution, desc = build_model(model_id)
-    print(desc, resolution)
-
-# print(build_mcunet('mcunet-512kb-in'), )
